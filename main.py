e0d34d86afa96d4af68bbaeddbe69650c0215533
wannacry3030 <wannacry.bat@proton.me>
2023-01-09 15:34:10 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from urllib import request
+from urllib import request, parse
 
-resposta = request.urlopen("http://g1.com.br")
+#criando um dicionario para fazer bypass do WAF
+cabecalho = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0", "Cookie": "cf_clearance=eiBP.XVHTCd.QZVWX9iQT7FPwx_3wB5dfcZtUUn.yqU-1673286953-0-150"}
+
+dados = {"user":"admin", "password":"senhafoda"}
+dados = parse.urlencode(dados).encode()
+
+req = request.Request("http://www.bancocn.com/admin/index.php", headers=cabecalho, data=dados)
+resposta = request.urlopen(req)
 html = resposta.read()
 print(html)
+
+
+
