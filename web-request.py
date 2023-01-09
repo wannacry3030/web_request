8ec548e65c550af2ba19bb3d77c4cbb0726943a0
wannacry3030 <wannacry.bat@proton.me>
2023-01-09 18:45:35 -0300

    new file:   __pycache__/urllib.cpython-311.pyc 	renamed:    urllib.py -> url-lib_request.py 	modified:   web-request.py

--- web-request.py
+++ web-request.py
@@ -1,6 +1,9 @@
 import requests
 
+cabecalho = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0", "Cookie": "cf_clearance=eiBP.XVHTCd.QZVWX9iQT7FPwx_3wB5dfcZtUUn.yqU-1673286953-0-150"}
 
-resposta = requests.get("http://bancocn.com")
+dados = {"user":"admin", "password":"senhafoda"}
+
+resposta = requests.post("http://www.bancocn.com/admin/index.php", headers=cabecalho, data=dados)
 html = resposta.text
 print(html)
